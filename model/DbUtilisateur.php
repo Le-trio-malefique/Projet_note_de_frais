e4d8cd072839635c72a33a254f96353851e7028f
jeremy <jeremylacroix21@outlook.fr>
2022-10-14 16:28:32 +0200

    DbUtilisateur

---
+++ model/DbUtilisateur.php
@@ -0,0 +1,20 @@
+<?php
+include 'connectPdo.php';
+
+class DbUtilisateur{
+	
+	public static function conectUser($email,$password)
+	{
+		$sql = "SELECT * FROM `utilisateur` WHERE Mail='$email' AND Mdp='$password'";
+		$objResultat = connectPdo::getObjPdo()->query($sql);
+		$result = $objResultat->fetchAll();
+		return $result;
+	}
+	public static function newUser($email,$password)
+	{
+		$sql = "insert into utilisateur (email, password) values ('".$email."','".$password."')";
+		connectPdo::getObjPdo()->exec($sql);
+	}
+}
+
+?>
