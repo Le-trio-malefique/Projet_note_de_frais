93c60199ddd746b46d82200baea37f970063f9e1
jeremy <jeremylacroix21@outlook.fr>
2022-10-14 16:40:13 +0200

    connect useer

---
+++ ctl/ctlUtilisateur.php
@@ -0,0 +1,43 @@
+<?php
+include './model/DbUtilisateur.php';
+
+$action = $_GET['action'];
+
+switch($action){
+    case 'formConnect':
+        include 'vue/vueConnexion/v_Connexion.php';
+        break;
+    
+    case 'formDeconnect':
+        session_unset();
+        header("Location:index.php");
+        break;
+
+    case 'formNew':
+        include 'vue/vueNewUser/v_NewUser.php';
+        break;
+
+    case 'NewUser':
+        if(isset($_POST['email']) && isset($_POST['password'])){
+            DbUtilisateur::newUser($_POST['email'],$_POST['password']);
+        }
+        break;
+
+    case 'connect':
+
+        if(isset($_POST['email']) && isset($_POST['password'])){
+
+            $result = DbUtilisateur::conectUser($_POST['email'],$_POST['password']);
+
+            if($result != null){
+                $_SESSION['login'] = $_POST['email'];
+                header("Location:index.php");
+            }
+            if($result == null){
+                header("Location:index.php?ctl=utilisateur&action=formConnect&msg=identifiant ou mots de passe incorrect");
+            }
+        }
+        
+        break;
+    
+}
---
+++ vue/vueConnexion/v_Connexion.php
@@ -0,0 +1,15 @@
+<div class="col-lg-4 m-auto">
+   <h1 class="text-center mt-3">CONNEXION</h1>
+   <form class="fluid-form" action="index.php?ctl=utilisateur&action=connect" method="post">
+      <!--<input type="hidden" name="ctl" value="utilisateur">
+      <input type="hidden" name="action" value="connect">-->
+      <input class="form-control mt-5" type="email" name="email" placeholder="Email" required>
+      <input class="form-control mt-2" type="password" name="password"  placeholder="Mot de passe" required>
+      <input class="form-control bg-dark text-white mt-5" type="submit">
+   </form>
+</div>
+<?php
+if(isset($_GET['msg'])){
+   echo "<h4 class='text-center mt-5 text-danger'>".$_GET['msg']."<h4>";
+}
+?>
