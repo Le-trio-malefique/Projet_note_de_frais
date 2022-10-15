d77f0d974daa0536c0be31890edaa1bf3bcf2911
jeremy <jeremylacroix21@outlook.fr>
2022-10-15 21:02:27 +0200

    x

--- vue/vueConnexion/v_Connexion.php
+++ vue/vueConnexion/v_Connexion.php
@@ -1,11 +1,11 @@
-<div class="col-lg-4 mx-auto rounded border border-secondary my-auto w-25" style="background-color : #fff7f0!important;">
+<div class="col-lg-4 mx-auto rounded border my-auto w-25 shadow" style="color: #1A4087;">
    <h1 class="text-center mt-3">CONNEXION</h1>
-   <form class="fluid-form" action="index.php?ctl=utilisateur&action=connect" method="post">
+   <form class="fluid-form text-center" action="index.php?ctl=utilisateur&action=connect" method="post">
       <!--<input type="hidden" name="ctl" value="utilisateur">
       <input type="hidden" name="action" value="connect">-->
-      <input class="form-control mt-5" type="email" name="email" placeholder="Email" required>
-      <input class="form-control mt-2" type="password" name="password"  placeholder="Mot de passe" required>
-      <input class="form-control bg-dark text-white mt-5" type="submit">
+      <input class="form-control mt-5 shadow" type="email" name="email" placeholder="Email" required style="color: #1A4087;">
+      <input class="form-control mt-2 shadow" type="password" name="password"  placeholder="Mot de passe" required style="color: #1A4087;">
+      <input class="form-control btn btn-light mt-5 border w-75 shadow" type="submit" style="color: #1A4087;">
    </form>
 </div>
 <?php
