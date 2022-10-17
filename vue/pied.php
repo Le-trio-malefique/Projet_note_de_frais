e92461fa37ba98c1149730cc87f594f73cbf6979
jeremy <jeremylacroix21@outlook.fr>
2022-10-17 16:37:40 +0200

    blank

--- vue/pied.php
+++ vue/pied.php
@@ -1,5 +1,5 @@
     <div class="row p-2 m-0 bg-white border-top fixed-bottom">
         <div class = "col-md-6 text-left"> © 2022 Institution Saint-Aspais </div>
-        <div class = "col-md-6 text-right"> <a href="https://institutionsaintaspais.fr/conditions-dutilisation/">Mentions légales et Conditions d’utilisation</a></div>
+        <div class = "col-md-6 text-right"> <a target="_blank"href="https://institutionsaintaspais.fr/conditions-dutilisation/">Mentions légales et Conditions d’utilisation</a></div>
     </div>
 </div>
