c48dd91e755b4f0398dab29864f24db691f826b7
tanguym77 <tanguy@marmier.me>
2022-10-17 16:27:11 +0200

    v3 footer

--- vue/pied.php
+++ vue/pied.php
@@ -1,4 +1,4 @@
-    <div class="row p-2 m-0 border-top fixed-bottom">
+    <div class="row p-2 m-0 bg-white border-top fixed-bottom">
         <div class = "col-md-6 text-left"> © 2022 Institution Saint-Aspais </div>
         <div class = "col-md-6 text-right"> <a href="https://institutionsaintaspais.fr/conditions-dutilisation/">Mentions légales et Conditions d’utilisation</a></div>
     </div>
