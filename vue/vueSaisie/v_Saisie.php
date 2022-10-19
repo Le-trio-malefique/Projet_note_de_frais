6c1842ff553cea6e1ecf3d94df01c4460fe2ca87
tanguym77 <tanguy@marmier.me>
2022-10-19 15:08:31 +0200

    footer

---
+++ vue/vueSaisie/v_Saisie.php
@@ -0,0 +1,24 @@
+<!-- CARD NEW NOTE DE FRAIS -->
+<div class="row h-100 w-100 bg-primary mx-auto d-flex justify-content-center">
+    <div class="col-md-7 bg-danger my-md-5">
+        <!-- TITRE -->
+        <div class="row border h-25">
+            <h4 class="text-left my-auto p-2">Note de frais</h4>
+        </div>
+        <!-- BODY -->
+        <div class="row border h-50 text-center">
+            <div class="my-auto mx-auto">
+                <h2>Aucune dépense effectuée en ce moment</h2>
+            </div>
+        </div>
+        <!-- BUTTONS -->
+        <div class="row border h-25 d-flex justify-content-around text-center">
+            <div class="col-lg-6 border p-3 my-auto"> 
+                <button type="button" class="btn btn-primary">+ Nouvelle note de frais 1</button>
+            </div>
+            <div class="col-lg-6 border p-3 my-auto">
+                <button type="button" class="btn btn-primary">+ Nouvelle note de frais 2</button>
+            </div>
+        </div>
+    </div>
+</div>
