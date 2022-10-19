c755eee770b6418635f002776bbe70a14e205e85
jeremy <jeremylacroix21@outlook.fr>
2022-10-19 14:25:17 +0200

    ok

--- vue/menu.php
+++ vue/menu.php
@@ -1,29 +1,17 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white shadow">
     <a class="navbar-brand" href="#"><h5><img class="img-fluid" src="vue/images/saintAspais.webp"></img></h5></a>
+    
+    <?php 
+        if(isset($_SESSION['login'])){
+    ?>
+
     <button class="navbar-toggler" style="border-color: #1A4087;" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon" style="color: #1A4087;"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-            <?php 
-                if(isset($_SESSION['login'])){
-            ?>
-            <li class="nav-item ml-auto active">
-                <a href="#profil" class="nav-link p-3" style="color: #1A4087;"><h3>Profil</h3><span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item ml-auto">
-                <a href="#saisie" class="nav-link p-3" style="color: #1A4087;"><h3>Saisie</h3></a>
-            </li>
-            <li class="nav-item ml-auto">
-                <a href="#historique"  class="nav-link p-3" style="color: #1A4087;"><h3>Historique</h3></a>
-            </li>
-            <?php
-                }
-            ?>
-            
-        </ul>
-    </div>
+    <?php 
+        }
+    ?>
 
     <div class="mr-0"> 
         <div class="dropdown">
@@ -47,11 +35,32 @@
                 <?php
                     }
                 ?>
-                <div class="dropdown-divider">
-                </div>
-                <li><a class="dropdown-item" href="index.php?ctl=utilisateur&action=formNew">Inscription</a></li>
             </ul>
         </div>
     </div>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        
+        <ul class="navbar-nav ml-auto">
+            <?php 
+                if(isset($_SESSION['login'])){
+            ?>
+            <li class="nav-item ml-auto active">
+                <a href="#profil" class="nav-link p-3" style="color: #1A4087;"><h3>Profil</h3><span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item ml-auto">
+                <a href="#saisie" class="nav-link p-3" style="color: #1A4087;"><h3>Saisie</h3></a>
+            </li>
+            <li class="nav-item ml-auto">
+                <a href="#historique"  class="nav-link p-3" style="color: #1A4087;"><h3>Historique</h3></a>
+            </li>
+            <?php
+                }
+            ?>
+            
+        </ul>
+    </div>
+
+    
 
 </nav>
