0b082054ce9764cb751c7440ab33de8df7e0db99
jeremy <jeremylacroix21@outlook.fr>
2022-10-20 10:13:50 +0200

    h-100 out

--- vue/vueSaisie/v_Saisie.php
+++ vue/vueSaisie/v_Saisie.php
@@ -1,9 +1,9 @@
 <!-- CARD NEW NOTE DE FRAIS -->
-<div class="row h-100 w-100 mx-auto d-flex justify-content-center">
+<div class="row w-100 mx-auto d-flex justify-content-center container-fluid mt-3" style="min-height : 100vh!important;">
     <!-- CARD BOX -->
     <div class="col-md-7 border rounded shadow my-md-5">
         <!-- TITRE -->
-        <div class="row h-25 border">
+        <div class="row border" style="min-height : 11vh!important;">
             <h4 class="text-left my-auto p-2 ml-3">Note de frais</h4>
         </div>
         <!-- TEXTE -->
@@ -16,15 +16,20 @@
         <?php
             }else{
         ?>
-        <div class="row h-50 text-center">
-            <div class="my-auto mx-5">
-                <h2>Aucune dépense en attente</h2>
+        <div class="row text-center" style="min-height : 70vh!important;">
+            <div class="mt-5 mx-5">
+                <ul>
+                    <li><p>coucou</p></li>
+                    <li><p>coucou</p></li>
+                    <li><p>coucou</p></li>
+                    <li><p>coucou</p></li>
+                </ul>
             </div>
         </div>
         <?php } ?>
 
         <!-- BUTTONS -->
-        <div class="row border h-25 d-flex justify-content-around text-center">
+        <div class="row border d-flex justify-content-around text-center" style="min-height : 11vh!important;">
             <div class="col-lg-6 p-3 my-auto"> 
                 <button type="button" class="btn btn-primary" href="https://google.fr"><i class="bi bi-plus-circle"></i> &nbsp Nouvelle note de frais 1</button>
             </div>
