39a47ae5c38558db2d5cb5b9aefc0230847351ea
jeremy <jeremylacroix21@outlook.fr>
2022-10-20 11:06:09 +0200

    fix ctlConnection

---
+++ ctl/ctlConnection.php
@@ -0,0 +1,35 @@
+<?php
+include './model/DbConnection.php';
+
+$action = $_GET['action'];
+
+switch($action){
+    case 'formConnect':
+        include 'vue/vueConnexion/v_Connexion.php';
+        break;
+    
+    case 'formDeconnect':
+        session_unset();
+        header("Location:index.php");
+        break;
+
+    case 'connect':
+
+        if(isset($_POST['email']) && isset($_POST['password'])){
+
+            $result = DbUtilisateur::conectUser($_POST['email'],$_POST['password']);
+
+            if($result != null){
+                $_SESSION['login'] = $_POST['email'];
+                $_SESSION['nom'] = $result[0]['Nom'];
+                $_SESSION['prenom'] = $result[0]['Prenom'];
+                $_SESSION['matricule'] = $result[0]['Mat'];
+                header("Location:index.php?ctl=notedefrais&action=saisie");
+            }
+            if($result == null){
+                header("Location:index.php?ctl=utilisateur&action=formConnect&msg=identifiant ou mots de passe incorrect");
+            }
+        }
+        
+        break;
+}
--- ctl/ctlUtilisateur.php
+++ ctl/ctlUtilisateur.php
@@ -1,41 +1,12 @@
 <?php
-include './model/DbUtilisateur.php';
 
 $action = $_GET['action'];
 
 switch($action){
-    case 'formConnect':
-        include 'vue/vueConnexion/v_Connexion.php';
-        break;
-    
-    case 'formDeconnect':
-        session_unset();
-        header("Location:index.php");
-        break;
-
     case 'profil':
         include 'vue/vueProfil/v_Profil.php';
         break;
-
-    case 'connect':
-
-        if(isset($_POST['email']) && isset($_POST['password'])){
-
-            $result = DbUtilisateur::conectUser($_POST['email'],$_POST['password']);
-
-            if($result != null){
-                $_SESSION['login'] = $_POST['email'];
-                $_SESSION['nom'] = $result[0]['Nom'];
-                $_SESSION['prenom'] = $result[0]['Prenom'];
-                $_SESSION['matricule'] = $result[0]['Mat'];
-                header("Location:index.php?ctl=notedefrais&action=saisie");
-            }
-            if($result == null){
-                header("Location:index.php?ctl=utilisateur&action=formConnect&msg=identifiant ou mots de passe incorrect");
-            }
-        }
-        
-        break;
+    
 
     case 'historique':
         include 'vue/vueProfil/v_Historique.php';
@@ -44,5 +15,5 @@ switch($action){
     case 'formvehicule':
         include 'vue/vueProfil/v_AjoutVehicule.php';
         break;
-    
+
 }
