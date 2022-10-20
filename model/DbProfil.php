6db4f3d0fd78ee783d71d0719343d9a5dc7d61e5
tanguym77 <tanguy@marmier.me>
2022-10-20 15:46:56 +0200

    AJOUT vehicule final

--- model/DbProfil.php
+++ model/DbProfil.php
@@ -5,7 +5,7 @@ class DbProfil{
 
     public static function ajoutvehicule($marque, $modele, $carburant, $cylindre, $id)
 	{
-        $sql = "INSERT INTO `vehicule` (`Id`, `Marque`, `Modele`, `Carburant`, `Cylindre`, `Id_utilisateur`) VALUES ('".$marque."', '".$modele."', '".$carburant."', '".$cylindre."', '".$id."')";
+        $sql = "INSERT INTO `vehicule` (`Marque`, `Modele`, `Carburant`, `Cylindre`, `Id_utilisateur`) VALUES ('".$marque."', '".$modele."', '".$carburant."', '".$cylindre."', '".$id."')";
 		connectPdo::getObjPdo()->exec($sql);
     }
 	
