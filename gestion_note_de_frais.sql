42b93ac5e3dedc26222ca1de57125a88cdfc77d9
tanguym77 <tanguy@marmier.me>
2022-10-20 13:51:45 +0200

    new Data base

--- gestion_note_de_frais.sql
+++ gestion_note_de_frais.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 17 oct. 2022 à 13:38
+-- Généré le : jeu. 20 oct. 2022 à 11:50
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -18,10 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `gestion_note_de_frais`
+-- Base de données : `gestion_note_de_frais_2`
 --
-CREATE DATABASE IF NOT EXISTS `gestion_note_de_frais` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `gestion_note_de_frais`;
+CREATE DATABASE IF NOT EXISTS `gestion_note_de_frais_2` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `gestion_note_de_frais_2`;
 
 -- --------------------------------------------------------
 
@@ -31,10 +31,14 @@ USE `gestion_note_de_frais`;
 
 DROP TABLE IF EXISTS `ligne_fc`;
 CREATE TABLE IF NOT EXISTS `ligne_fc` (
-  `Id_ndf_fc` int(20) NOT NULL AUTO_INCREMENT,
+  `Id` int(20) NOT NULL AUTO_INCREMENT,
   `Montant` float NOT NULL,
   `Justificatif` varchar(20) NOT NULL,
-  PRIMARY KEY (`Id_ndf_fc`)
+  `Id_ndf` int(20) NOT NULL,
+  `Statut` varchar(20) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk_3` (`Id_ndf`),
+  KEY `fk_status` (`Statut`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -45,10 +49,14 @@ CREATE TABLE IF NOT EXISTS `ligne_fc` (
 
 DROP TABLE IF EXISTS `ligne_fk`;
 CREATE TABLE IF NOT EXISTS `ligne_fk` (
-  `Id_ndf_fk` int(20) NOT NULL AUTO_INCREMENT,
+  `Id` int(20) NOT NULL AUTO_INCREMENT,
   `Montant` float NOT NULL,
   `Justificatif` varchar(20) NOT NULL,
-  PRIMARY KEY (`Id_ndf_fk`)
+  `Id_ndf` int(20) NOT NULL,
+  `Statut` varchar(20) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk_idndf` (`Id_ndf`),
+  KEY `fk_status_2` (`Statut`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -59,28 +67,35 @@ CREATE TABLE IF NOT EXISTS `ligne_fk` (
 
 DROP TABLE IF EXISTS `note_de_frais`;
 CREATE TABLE IF NOT EXISTS `note_de_frais` (
-  `Id` int(20) NOT NULL AUTO_INCREMENT,
-  `Montant` float DEFAULT NULL,
-  `Status` varchar(20) DEFAULT NULL,
-  `Date_ndf` varchar(20) DEFAULT NULL,
-  `Id_utilisateur` int(20) DEFAULT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `Id_utilisateur` (`Id_utilisateur`),
-  KEY `Status` (`Status`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `Id_ndf` int(20) NOT NULL AUTO_INCREMENT,
+  `Date` date DEFAULT NULL,
+  `Id_FC` int(20) DEFAULT NULL,
+  `Id_FK` int(20) DEFAULT NULL,
+  `Id_Utilisateur` int(20) DEFAULT NULL,
+  PRIMARY KEY (`Id_ndf`),
+  KEY `fk_2` (`Id_Utilisateur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `status`
+-- Structure de la table `statut`
 --
 
-DROP TABLE IF EXISTS `status`;
-CREATE TABLE IF NOT EXISTS `status` (
-  `Id` int(20) NOT NULL AUTO_INCREMENT,
-  `Libelle` varchar(20) NOT NULL,
-  PRIMARY KEY (`Id`,`Libelle`)
+DROP TABLE IF EXISTS `statut`;
+CREATE TABLE IF NOT EXISTS `statut` (
+  `Label` varchar(20) NOT NULL,
+  PRIMARY KEY (`Label`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `statut`
+--
+
+INSERT INTO `statut` (`Label`) VALUES
+('Accepté'),
+('En attente'),
+('Refusé');
 
 -- --------------------------------------------------------
 
@@ -92,20 +107,20 @@ DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
   `Id` int(20) NOT NULL AUTO_INCREMENT,
   `Mat` int(20) NOT NULL,
-  `Nom` varchar(20) DEFAULT NULL,
-  `Prenom` varchar(20) DEFAULT NULL,
-  `Mail` varchar(20) DEFAULT NULL,
-  `Mdp` varchar(20) DEFAULT NULL,
-  `Admin` tinyint(1) DEFAULT NULL,
+  `Nom` varchar(20) NOT NULL,
+  `Prenom` varchar(20) NOT NULL,
+  `Mail` varchar(20) NOT NULL,
+  `Mdp` varchar(20) NOT NULL,
+  `Admin` tinyint(1) NOT NULL,
   PRIMARY KEY (`Id`,`Mat`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `utilisateur`
 --
 
 INSERT INTO `utilisateur` (`Id`, `Mat`, `Nom`, `Prenom`, `Mail`, `Mdp`, `Admin`) VALUES
-(1, 6448723, 'Pignion', 'Louis', 'Louis@Pignion.fr', 'mdp', 1);
+(1, 6448723, 'Pignion', 'Louis', 'Louis@Pignion.fr', 'mdp', 0);
 
 -- --------------------------------------------------------
 
@@ -120,8 +135,9 @@ CREATE TABLE IF NOT EXISTS `vehicule` (
   `Modele` varchar(20) NOT NULL,
   `Carburant` varchar(20) NOT NULL,
   `Cylindre` varchar(20) NOT NULL,
-  `Mat_utilisateur` varchar(20) NOT NULL,
-  PRIMARY KEY (`Id`)
+  `Id_utilisateur` int(20) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `Id_utilisateur` (`Id_utilisateur`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -129,11 +145,30 @@ CREATE TABLE IF NOT EXISTS `vehicule` (
 --
 
 --
+-- Contraintes pour la table `ligne_fc`
+--
+ALTER TABLE `ligne_fc`
+  ADD CONSTRAINT `fk_3` FOREIGN KEY (`Id_ndf`) REFERENCES `note_de_frais` (`Id_ndf`),
+  ADD CONSTRAINT `fk_status` FOREIGN KEY (`Statut`) REFERENCES `statut` (`Label`);
+
+--
+-- Contraintes pour la table `ligne_fk`
+--
+ALTER TABLE `ligne_fk`
+  ADD CONSTRAINT `fk_idndf` FOREIGN KEY (`Id_ndf`) REFERENCES `note_de_frais` (`Id_ndf`),
+  ADD CONSTRAINT `fk_status_2` FOREIGN KEY (`Statut`) REFERENCES `statut` (`Label`);
+
+--
 -- Contraintes pour la table `note_de_frais`
 --
 ALTER TABLE `note_de_frais`
-  ADD CONSTRAINT `note_de_frais_ibfk_1` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`Id`),
-  ADD CONSTRAINT `note_de_frais_ibfk_2` FOREIGN KEY (`Id`) REFERENCES `ligne_fc` (`Id_ndf_fc`);
+  ADD CONSTRAINT `fk_2` FOREIGN KEY (`Id_Utilisateur`) REFERENCES `utilisateur` (`Id`);
+
+--
+-- Contraintes pour la table `vehicule`
+--
+ALTER TABLE `vehicule`
+  ADD CONSTRAINT `vehicule_ibfk_1` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`Id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
