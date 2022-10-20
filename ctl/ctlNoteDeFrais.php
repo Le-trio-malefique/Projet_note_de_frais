dc680d63e46f31880e6a932b6fb9cb5057217344
jeremy <jeremylacroix21@outlook.fr>
2022-10-20 15:41:13 +0200

    vue ndf / saisie ndf

--- ctl/ctlNoteDeFrais.php
+++ ctl/ctlNoteDeFrais.php
@@ -6,13 +6,13 @@ $action = $_GET['action'];
 switch($action){
     case 'lister':
         $result = DbNoteDeFrais::lister($_SESSION['id']);
-        include 'vue/vueSaisie/v_Saisie.php';
+        include 'vue/vueSaisie/v_ListeNdf.php';
         break;
 
     case 'newNote':
         DbNoteDeFrais::newNote($_SESSION['id']);
         $result = DbNoteDeFrais::lister($_SESSION['id']);
-        include 'vue/vueSaisie/v_Saisie.php';
+        include 'vue/vueSaisie/v_ListeNdf.php';
         break;
     
     case 'saisie':
