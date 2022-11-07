07b05a6d60a6d07b99ed573dfa9f7bb0a594076d
Yvanne ROSAT <rosatyvann@gmail.com>
2022-11-07 15:51:38 +0100

    Conect DataBase->hostinger

--- model/connectPdo.php
+++ model/connectPdo.php
@@ -10,8 +10,8 @@ class connectPdo
 		 
 		if(!isset(self::$db))
 		{
-			self::$db = new PDO('mysql:dbname=gestion_note_de_frais;host=localhost', 'root', '');
-			//self::$db = new PDO('mysql:dbname=u479250329_note_de_frais;host=ftp://yvannerosat.com', 'u479250329.notedefrais.store', 'Rootroot10');
+			//self::$db = new PDO('mysql:dbname=gestion_note_de_frais;host=localhost', 'root', '');
+			self::$db = new PDO('mysql:dbname=u479250329_note_de_frais;host=notedefrais.store', 'u479250329_user', 'Rootroot10');
 			self::$db ->query('SET NAMES utf8');
 			self::$db->query('SET CHARACTER SET utf8');
 		} 
