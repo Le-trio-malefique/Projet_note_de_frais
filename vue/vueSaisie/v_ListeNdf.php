ba31d267a2d8e76ea933bf903809858e2f478b9c
jerermy-lacroix <jeremylacroix21@outlook.fr>
2022-11-09 11:23:00 +0100

    bouton modifier

--- vue/vueSaisie/v_ListeNdf.php
+++ vue/vueSaisie/v_ListeNdf.php
@@ -18,7 +18,7 @@
                                     ".$row['Date']."
                                 </td>
                                 <td class='p-3 d-flex-lg justify-content-center'>
-                                    <a class='btn btn-primary mx-auto w-100' style='max-width : 200px!important;' href='index.php?ctl=notedefrais&action=saisie&id=".$row['Id_ndf']."'>Saisir des frais</a>
+                                    <a class='btn btn-primary mx-auto w-100' style='max-width : 200px!important;' href='index.php?ctl=notedefrais&action=saisie&id=".$row['Id_ndf']."'>Modifier</a>
                                     <form class='mt-2 p-0' action='index.php?ctl=notedefrais&action=supprimer' method='post'>
                                         <input type='hidden' name='idNdf' value='".$row['Id_ndf']."'>
                                         <input type='submit' class='btn btn-danger mx-auto w-100' value='Suprimer' style='max-width : 200px!important;'>
