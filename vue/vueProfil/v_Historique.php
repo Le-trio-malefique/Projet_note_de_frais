cd851479513357d9c4f86ed0c3c12abdeaf07eb1
tanguym77 <tanguy@marmier.me>
2022-11-17 13:21:20 +0100

    ne pas récup (incomplet)

--- vue/vueProfil/v_Historique.php
+++ vue/vueProfil/v_Historique.php
@@ -1,9 +1,8 @@
 <?php 
 print_r($_SESSION['ndf']); 
-foreach ($_SESSION['ndf'] as $id => $idndf) {
+foreach ($_SESSION['ndf'] as $key => $idndf) {
     echo "<br>";
-    echo $idndf[0];
-    echo $id[0]
+    echo $idndf[0],$idndf[1];
 }
 
 ?>
