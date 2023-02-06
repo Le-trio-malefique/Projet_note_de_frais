a6cc607b8e098ff409b07a0819b463025a049f4e
Yvanne ROSAT <rosatyvann@gmail.com>
2023-02-06 14:20:07 +0100

    Correct_bug_connect

--- ctl/ctlConnection.php
+++ ctl/ctlConnection.php
@@ -19,34 +19,8 @@ switch ($action) {
         break;
 
     case 'connect':
-
-        if(isset($_POST['email']) && isset($_POST['password'])){
-
-            $result = DbConnection::connectUser($_POST['email'],$_POST['password']);
-            
-            if($result != null){
-                $_SESSION['login'] = $_POST['email'];
-                $_SESSION['nom'] = $result[0]['Nom'];
-                $_SESSION['prenom'] = $result[0]['Prenom'];
-                $_SESSION['matricule'] = $result[0]['Mat'];
-                $_SESSION['id'] = $result[0]['Id'];
-                $_SESSION['Admin'] = $result[0]['Admin'];
-                $result_v = DbConnection::getVehicule($result[0]['Id']);
-                if($result_v != null){
-                    $_SESSION['vehicule'] = [$result_v[0]['Marque'], $result_v[0]['Modele'], $result_v[0]['Carburant'], $result_v[0]['Cylindre']];
-                }
-                if($_SESSION['Admin'] == 0){
-                    header("Location:index.php?ctl=notedefrais&action=lister");
-                }
-                else{ 
-                    header("Location:index.php?ctl=gestionnaire&action=profilAdmin");
-                }
-                
-            }
-            if($result == null){
-                header("Location:index.php?ctl=Connection&action=formConnect&msg=identifiant ou mots de passe incorrect");
-            }
-        }
+        // function verify connection user and admin
+        DbConnection::verifyUserConnection();
         
         break;
 }
